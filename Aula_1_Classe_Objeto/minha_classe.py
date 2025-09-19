f96d35faa984493796d822494942f9732a10c890
Lucas <lucasvieiraribeiro2055@gmail.com>
2025-09-18 22:55:07 -0300

    Estudo sobre classes e construtores

---
+++ Aula_1_Classe_Objeto/minha_classe.py
@@ -0,0 +1,11 @@
+class MinhaClasse:
+    def __init__(self):
+        self.atributo_1 = "meu atributo"
+
+    def metodo_1(self):
+        print("minha ação")
+        return "ola mundo"
+
+minha_classe=  MinhaClasse()
+
+minha_classe.metodo_1()
