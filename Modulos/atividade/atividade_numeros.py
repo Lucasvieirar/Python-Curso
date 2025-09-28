87c75e7c7ac5ffd5a6026a06ab0df4b021ed6135
Lucas <lucasvieiraribeiro2055@gmail.com>
2025-09-28 19:35:10 -0300

    Cria funções para operações numericas

---
+++ Modulos/atividade/atividade_numeros.py
@@ -0,0 +1,20 @@
+def somar(x, y):
+    s = x + y
+    print(f"a soma de {x} e {y} é {s}")
+
+def subtrair(x, y):
+    s = x - y
+    print(f"a subtração de {x} e {y} é {s}")
+
+def mutiplica(x, y):
+    s = x * y
+    print(f"a multiplicação de {x} e {y} é {s}")
+
+def dividir(x, y):
+    s = x / y
+    print(f"a divisão de {x} e {y} é {s}")
+
+num1 = int(input("Digite um numero: "))
+num2 = int(input("Digite outro valor: "))
+somar(num1, num2)
+
