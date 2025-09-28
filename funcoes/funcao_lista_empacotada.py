88613f47f9c6cbce8361638314927559ec83c122
Lucas <lucasvieiraribeiro2055@gmail.com>
2025-09-28 17:50:39 -0300

    cria função soma empacotada

---
+++ funcoes/funcao_lista_empacotada.py
@@ -0,0 +1,9 @@
+def somar( * valores):
+    s = 0
+    for valor in valores:
+        s += valor
+    print(f'Somando os valores{valores} temos {s}')
+
+somar(5, 2)
+somar(2, 9, 1)
+somar(8, 5, 10, 2)
