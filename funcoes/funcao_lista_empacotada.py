164e961675c149896611000b35cd42877972e31c
Lucas <lucasvieiraribeiro2055@gmail.com>
2025-09-28 18:20:24 -0300

    cria função fatorial

--- funcoes/funcao_lista_empacotada.py
+++ funcoes/funcao_lista_empacotada.py
@@ -2,7 +2,7 @@ def somar( * valores):
     s = 0
     for valor in valores:
         s += valor
-    print(f'Somando os valores{valores} temos {s}')
+    print(f'Somando os valores {valores} temos {s}')
 
 somar(5, 2)
 somar(2, 9, 1)
