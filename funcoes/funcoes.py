d34590bd96864269eccaa0380f5d80ba6899702b
Lucas <lucasvieiraribeiro2055@gmail.com>
2025-09-28 17:09:52 -0300

    estuda funções

---
+++ funcoes/funcoes.py
@@ -0,0 +1,12 @@
+#otimizar tarefas repetitivas
+# df funco():
+
+print('-' *30)
+print(' CURSO DE PYTHON')
+print('-' *30)                 #SEM A FUNÇÃO
+print(' APRENDER PYTHON')
+print('-' *30)
+print('LUCAS')
+print('-' *30)  
+
+
