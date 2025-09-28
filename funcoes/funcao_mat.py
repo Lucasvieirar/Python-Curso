15cb5456274348b4d77dcc0668691c875e72ec97
Lucas <lucasvieiraribeiro2055@gmail.com>
2025-09-28 17:26:06 -0300

    cria função soma

--- funcoes/funcao_mat.py
+++ funcoes/funcao_mat.py
@@ -1,16 +1,23 @@
 
+# a = 4
+# b = 5
+# s = a + b
+# print(s)
 
-a = 4
-b = 5
-s = a + b
-print(s)
+# a = 8
+# b = 9
+# s = a + b
+# print(s)
 
-a = 8
-b = 9
-s = a + b
-print(s)
+# a = 2
+# b = 1
+# s = a + b
+# print(s)
 
-a = 2
-b = 1
-s = a + b
-print(s)
+def somar(a, b):
+    s = a + b
+    print(s)
+
+somar(4 , 5)
+somar(8, 9)
+somar(2, 1)
