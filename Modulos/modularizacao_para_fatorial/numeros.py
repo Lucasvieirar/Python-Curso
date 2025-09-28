c96e61981769f5f6284698331a323ca8ec153065
Lucas <lucasvieiraribeiro2055@gmail.com>
2025-09-28 18:56:58 -0300

    cria pacotes

--- Modulos/modularizacao_para_fatorial/numeros.py
+++ Modulos/modularizacao_para_fatorial/numeros.py
@@ -1,6 +1,15 @@
-import utils 
+# import utils
+# num = int(input("Digite um valor: "))
+
+# fat = utils.fatorial(num)
+
+# print(f'O fatorial de {num} é {fat}')
+
+from utilidades import numeros
 num = int(input("Digite um valor: "))
 
-fat = utils.fatorial(num)
+fat = numeros.fatorial(num)
 
 print(f'O fatorial de {num} é {fat}')
+
+
