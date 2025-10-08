7b88bbd861eac93d98cbbdc2c0f653745d60068c
Lucas <lucasvieiraribeiro2055@gmail.com>
2025-10-07 21:23:50 -0300

    Estudo principio Open

--- Classes/Aula_9_Solid_O/O_open.py
+++ Classes/Aula_9_Solid_O/O_open.py
@@ -2,21 +2,42 @@
 #entidades de software, dever ser abertar para extensão
 #mas fechadas para modificação
 
+# class Circo:
+#     def apresentar(self, comando: int):
+#         if comando == 1:
+#             self.__show_palhaco()
+#                           #preciso escalonar vou aumentar exponencionalmente o tamanho do codigo
+#         if comando == 2:
+#             self.__show_malabarista()
+
+#     def __show_palhaco(self):
+#         print("O palhaço esta apresentando seu show")
+
+#     def __show_malabarista(self):
+#         print("O malabarista esta apresentando seu show")
+
+# circo = Circo()   
+
+# comando = 2
+# circo.apresentar(comando)
+
+class Artista:
+    def __init__(self, tipo: str):
+        self.tipo = tipo
+
+    def apresentar_show(self):
+        print(f"O {self.tipo} esta apresentando seu show")
+
 class Circo:
-    def apresentar(self, comando: int):
-        if comando == 1:
-            self.__show_palhaco()
+    def apresentar(self, artista: Artista):
+        print("O circo esta abrindo!")
+        artista.apresentar_show()
+        print("O publico aplaude!")
+        
+circo = Circo()
 
-        if comando == 2:
-            self.__show_malabarista()
+palhaco = Artista("Palhaço")
+magico = Artista("Magico")
+malabarista =Artista("malabarista")
 
-    def __show_palhaco(self):
-        print("O palhaço esta apresentando seu show")
-
-    def __show_malabarista(self):
-        print("O malabarista esta apresentando seu show")
-
-circo = Circo()   
-
-comando = 2
-circo.apresentar(comando)
+circo.apresentar(malabarista)
