996221ab5c5bddd2aee745afdfb1ad873897664d
Lucasvieirar <lucasvieiraribeiro2055@gmail.com>
2025-11-17 00:01:20 -0300

    Composição

---
+++ Classes/Aula_21_composicao/composicao.py
@@ -0,0 +1,19 @@
+#compor uma classe atraves de outra
+
+class Select:
+    def by_id(self) -> None:
+        print("Selecionando um elemento no BD")
+
+class Insert:
+    def inser_value(self) -> None:
+        print("inserindo um valor no BD")
+
+class Repositorio:
+    def __init__(self) -> None:
+        self.__select = Select() #Composição
+        self.__insert = Insert()
+    
+    def select_by_id(self, id: int) -> any:
+        self.__select.by_id()
+repo = Repositorio()
+repo.select_by_id(123)
