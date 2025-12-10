0792ebfe485df34429a42949933ec8809f7b6b5e
Lucasvieirar <lucasvieiraribeiro2055@gmail.com>
2025-12-09 23:28:21 -0300

    metodos mágicos

--- Métodos Mágicos/vetor.py
+++ Métodos Mágicos/vetor.py
@@ -11,6 +11,8 @@
 class Vetor:
     def __init__(self, n1):
         self.n1 = 1
+        self.vetor = [1,2,3,4,5]
+        self.pessoa = {"Nome": "caio"}
     
     def __add__(self, n):
         if not isinstance(n, int):
@@ -36,6 +38,24 @@ class Vetor:
         if not isinstance(n, int):
             n = float(n)
         return self.n1 % n
+    def __eq__(self, n):
+        return True if self.n1 == n else False
+    def __neg__(self):
+        return list(map(lambda x: -x if x > 0 else x, self.vetor))
+    def __getitem__(self, key):
+        if not key in self.pessoa.keys():
+               self.pessoa[key] = None
+        return self.pessoa[key]
+    def __setitem__(self, key, value):
+        self.pessoa[key] = value
+    def __delitem__(self, key):
+        del self.pessoa[key]
+    def _call__(seçf, *args, **kwargs):
+        print("Fui chamdado", args)
 
-x = Vetor(10)
-print(x * 2)
+x = Vetor(2)
+x(1,2)
+
+
+print(x.pessoa)
+
