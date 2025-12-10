8a4fe281be3939c763485d85e2c6839bc845eb73
Lucasvieirar <lucasvieiraribeiro2055@gmail.com>
2025-12-09 22:42:18 -0300

    metodos especiais para oepraçãoes matematicas

--- Métodos Mágicos/vetor.py
+++ Métodos Mágicos/vetor.py
@@ -9,11 +9,33 @@
 # print(type(Vetor))
 
 class Vetor:
-    def __init__(self):
+    def __init__(self, n1):
         self.n1 = 1
     
     def __add__(self, n):
+        if not isinstance(n, int):
+            n = float(n)
         return self.n1 + n
+    
+    def __sub__(self, n):
+        if not isinstance(n, int):
+            n = float(n)
+        return self.n1 - n
+    
+    def __mul__(self, n):
+        if not isinstance(n, int):
+            n = int(n)
+        return self.n1 * n
+    
+    def __pow__(self, n):
+        if not isinstance(n, int):
+            n = float(n)
+        return self.n1 ** n
+    
+    def __mod__(self, n):
+        if not isinstance(n, int):
+            n = float(n)
+        return self.n1 % n
 
-x = Vetor()
-print( x + 3)
+x = Vetor(10)
+print(x * 2)
